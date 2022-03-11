40acf4856870c01b3504db43789851638f91f445
ANASZAIDULLAH <bitm-f19-025@superior.edu.pk>
2022-03-12 00:42:37 +0500

    Sqlite

--- app/src/main/java/com/example/nlag_login/MainActivity.java
+++ app/src/main/java/com/example/nlag_login/MainActivity.java
@@ -5,20 +5,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText username, password;
+    Button login, signup;
+    DBHelper DB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void openSignUp(View view) {
-        startActivity(new Intent(this,SignUp.class));
-    }
+        username = (EditText) findViewById(R.id.username1);
+        password = (EditText) findViewById(R.id.password1);
+        login = (Button) findViewById(R.id.log1);
+        signup = (Button) findViewById(R.id.signp);
+        DB = new DBHelper(this);
 
-    public void openMain_Menu(View view) {
-        startActivity(new Intent(this, Main_Menu.class));
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if(user.equals("")||pass.equals(""))
+                    Toast.makeText(MainActivity.this,"Please Enter Fields",Toast.LENGTH_SHORT).show();
+                else {
+                    Boolean checkuserpass = DB.checkusernamepassword(user,pass);
+                    if(checkuserpass==true){
+                        Toast.makeText(MainActivity.this,"Sign in Successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(),Main_Menu.class);
+                        startActivity(intent);
+
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this,"Invalid Credentials", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/nlag_login/SignUp.java
+++ app/src/main/java/com/example/nlag_login/SignUp.java
@@ -5,19 +5,72 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
 
+    EditText username, password, confirmp;
+    Button signup, login;
+    DBHelper DB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+    username = (EditText) findViewById(R.id.username);
+    password = (EditText) findViewById(R.id.password);
+    confirmp = (EditText) findViewById(R.id.confirmp);
+    signup = (Button) findViewById(R.id.sign);
+    login = (Button) findViewById(R.id.log);
+DB = new DBHelper(this);
+
+    signup.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            String user = username.getText().toString();
+            String pass = password.getText().toString();
+            String confirm = confirmp.getText().toString();
+
+            if(user.equals("") ||pass.equals("") || confirm.equals(""))
+                Toast.makeText(SignUp.this,"Please Enter all the fields", Toast.LENGTH_SHORT).show();
+            else {
+                if (pass.equals(confirm)){
+                    Boolean checkuser = DB.checkusername(user);
+                    if(checkuser==false){
+                        Boolean insert = DB.insertData(user, pass);
+                        if (insert==true){
+                            Toast.makeText(SignUp.this,"Registered Successfully",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(),Main_Menu.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(SignUp.this, "Registeration Failed", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                }
+            }
+        }
+    });
+ login.setOnClickListener(new View.OnClickListener() {
+     @Override
+     public void onClick(View view) {
+         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+         startActivity(intent);
+     }
+ });
+
+
+
+
+
+
     }
 
 
-    public void openMainActivity(View view) {
-        startActivity(new Intent(this,MainActivity.class));
-    }
+
 
 }
