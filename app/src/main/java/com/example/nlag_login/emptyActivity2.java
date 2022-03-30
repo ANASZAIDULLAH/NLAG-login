90051ce2fccd399165200b68365939b6e2005be6
ANASZAIDULLAH <bitm-f19-025@superior.edu.pk>
2022-03-30 14:31:48 +0500

    DashBoard Design

---
+++ app/src/main/java/com/example/nlag_login/emptyActivity2.java
@@ -0,0 +1,14 @@
+package com.example.nlag_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class emptyActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty2);
+    }
+}
